93a72fc4ec5606d99f3d7b32622cabecede8b15b
Marcin Kostrzewa <marckostrzewa@gmail.com>
2024-04-25 12:56:42 +0200

    parallel impl

--- prover/barycentric/barycentric_test.go
+++ prover/barycentric/barycentric_test.go
@@ -35,6 +35,30 @@ func TestBarycentricCircuit(t *testing.T) {
 	)
 }
 
+func barycentricFormula(rou, modulus, target *big.Int, interpolated []big.Int) *big.Int {
+	d := big.NewInt(int64(len(interpolated)))
+
+	// First term
+	firstTerm := new(big.Int).Exp(target, d, modulus)
+	firstTerm.Sub(firstTerm, big.NewInt(1))
+	firstTerm.Div(firstTerm, d)
+
+	// Second term
+	secondTerm := big.NewInt(0)
+	for degree := range len(interpolated) {
+		i := big.NewInt(int64(degree))
+		omegaToI := new(big.Int).Exp(rou, i, modulus)
+		numerator := new(big.Int).Mul(&interpolated[degree], omegaToI)
+		denominator := new(big.Int).Sub(target, omegaToI)
+		denominatorInverse := new(big.Int).ModInverse(denominator, modulus)
+		term := new(big.Int).Mul(numerator, denominatorInverse)
+		secondTerm.Add(secondTerm, term)
+	}
+
+	res := new(big.Int).Mul(firstTerm, secondTerm)
+	return res.Mod(res, modulus)
+}
+
 func TestDankradBarycentricCircuit(t *testing.T) {
 	assert := test.NewAssert(t)
 
@@ -51,7 +75,7 @@ func TestDankradBarycentricCircuit(t *testing.T) {
 
 	foo, _ := new(big.Int).SetString("10238227357739495823651030575849232062558860180284477541189508159991286009131", 10)
 
-	// wiemy: foo^(2^32) = 1
+	// we have: foo^(2^32) = 1
 	// (foo^2)^(2^31) = foo ^ (2 * 2^31) = foo ^ (2 ^ 32) = 1
 	// (foo^(2^30))^4 = foo ^ (4 * 2 ^ 30) = foo ^ (2^2 * 2^30) = foo ^ (2 ^ 32) = 1
 
@@ -66,16 +90,27 @@ func TestDankradBarycentricCircuit(t *testing.T) {
 		fmt.Println(new(big.Int).Exp(foo, big.NewInt(int64(i)), modulus))
 	}
 
+	interpolatedBI := make([]big.Int, 4)
+	for i := range 4 {
+		interpolatedBI[i] = *new(big.Int).Exp(foo, big.NewInt(int64(i*3)), modulus)
+		fmt.Println(&interpolatedBI[i])
+	}
+
+	fmt.Println(barycentricFormula(foo, modulus, big.NewInt(3), interpolatedBI))
+
 	interpolated := make([]frontend.Variable, 4)
 	for i := range 4 {
 		interpolated[i] = new(big.Int).Exp(foo, big.NewInt(int64(i*3)), modulus)
 		fmt.Println(interpolated[i])
 	}
 
+	unexpectedlyExpected, ok := new(big.Int).SetString("-2097435007005047619177909620327438633507622100021105512905054290570919745945600", 10)
+	assert.True(ok)
+
 	assignment := CircuitDankrad[emulated.BLS12381Fr]{
 		Omega:             *foo,
 		YNodes:            interpolated,
-		TargetPoint:       3,
+		TargetPoint:       *unexpectedlyExpected,
 		InterpolatedPoint: 27,
 	}
 
