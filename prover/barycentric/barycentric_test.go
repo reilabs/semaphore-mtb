74419ae702268a5c8fcfa1aef02bce05064c314d
Marcin Kostrzewa <marckostrzewa@gmail.com>
2024-04-25 12:39:44 +0200

    find out the correct root of unity

--- prover/barycentric/barycentric_test.go
+++ prover/barycentric/barycentric_test.go
@@ -1,6 +1,7 @@
 package barycentric
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
@@ -15,8 +16,8 @@ func TestBarycentricCircuit(t *testing.T) {
 	assert := test.NewAssert(t)
 
 	circuit := Circuit[emulated.BLS12381Fr]{
-		XNodes:            make([]frontend.Variable, 4),
-		YNodes:            make([]frontend.Variable, 4),
+		XNodes: make([]frontend.Variable, 4),
+		YNodes: make([]frontend.Variable, 4),
 	}
 
 	assignment := Circuit[emulated.BLS12381Fr]{
@@ -31,7 +32,7 @@ func TestBarycentricCircuit(t *testing.T) {
 		test.WithBackends(backend.GROTH16),
 		test.WithCurves(ecc.BLS12_381),
 		test.WithValidAssignment(&assignment),
-		)
+	)
 }
 
 func TestDankradBarycentricCircuit(t *testing.T) {
@@ -42,10 +43,38 @@ func TestDankradBarycentricCircuit(t *testing.T) {
 		YNodes: make([]frontend.Variable, 4),
 	}
 
+	modulus, ok := new(big.Int).SetString("52435875175126190479447740508185965837690552500527637822603658699938581184513", 10)
+	assert.True(ok)
+	modulus2, ok := new(big.Int).SetString("73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001", 16)
+	assert.True(ok)
+	assert.Equal(*modulus, *modulus2)
+
+	foo, _ := new(big.Int).SetString("10238227357739495823651030575849232062558860180284477541189508159991286009131", 10)
+
+	// wiemy: foo^(2^32) = 1
+	// (foo^2)^(2^31) = foo ^ (2 * 2^31) = foo ^ (2 ^ 32) = 1
+	// (foo^(2^30))^4 = foo ^ (4 * 2 ^ 30) = foo ^ (2^2 * 2^30) = foo ^ (2 ^ 32) = 1
+
+	for range 30 {
+		fmt.Println(foo)
+		foo.Mul(foo, foo)
+		foo.Mod(foo, modulus)
+	}
+	fmt.Println(foo)
+
+	for i := range 17 {
+		fmt.Println(new(big.Int).Exp(foo, big.NewInt(int64(i)), modulus))
+	}
+
+	interpolated := make([]frontend.Variable, 4)
+	for i := range 4 {
+		interpolated[i] = new(big.Int).Exp(foo, big.NewInt(int64(i*3)), modulus)
+		fmt.Println(interpolated[i])
+	}
+
 	assignment := CircuitDankrad[emulated.BLS12381Fr]{
-		// TODO this will be 10238227357739495823651030575849232062558860180284477541189508159991286009131
-		Omega:  *big.NewInt(2), // 1 2 4 8
-		YNodes:            []frontend.Variable{1, 8, 64, 512},
+		Omega:             *foo,
+		YNodes:            interpolated,
 		TargetPoint:       3,
 		InterpolatedPoint: 27,
 	}
