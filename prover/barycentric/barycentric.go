300be2885a05889577a6da581666df80af304e28
Wojciech Zmuda <zmuda.w@gmail.com>
2024-04-26 22:53:20 +0200

    remove old impl

--- prover/barycentric/barycentric.go
+++ prover/barycentric/barycentric.go
@@ -7,41 +7,6 @@ import (
 	"github.com/consensys/gnark/std/math/emulated"
 )
 
-type Circuit[T emulated.FieldParams] struct {
-	// Inputs (private)
-	XNodes      []frontend.Variable
-	YNodes      []frontend.Variable
-	TargetPoint frontend.Variable
-
-	// Output
-	InterpolatedPoint frontend.Variable `gnark:",public"`
-}
-
-func (circuit *Circuit[T]) Define(api frontend.API) error {
-	field, err := emulated.NewField[T](api)
-	if err != nil {
-		return err
-	}
-
-	// Convert frontend.Variables to field elements
-	api.AssertIsEqual(len(circuit.XNodes), len(circuit.YNodes))
-	xNodesFe := make([]emulated.Element[T], len(circuit.XNodes))
-	yNodesFe := make([]emulated.Element[T], len(circuit.YNodes))
-	for i := range circuit.XNodes {
-		xNodesFe[i] = variableToFieldElement(field, api, circuit.XNodes[i])
-		yNodesFe[i] = variableToFieldElement(field, api, circuit.YNodes[i])
-	}
-	targetPointFe := variableToFieldElement(field, api, circuit.TargetPoint)
-	interpolatedPointFe := variableToFieldElement(field, api, circuit.InterpolatedPoint)
-
-	weights := calculateWeights[T](field, xNodesFe)
-	interpolatedPointCalculated := barycentricPolynomial[T](field, weights, xNodesFe, yNodesFe, targetPointFe)
-
-	field.AssertIsEqual(&interpolatedPointFe, &interpolatedPointCalculated)
-
-	return nil
-}
-
 func variableToFieldElement[T emulated.FieldParams](
 	field *emulated.Field[T],
 	api frontend.API,
@@ -50,42 +15,7 @@ func variableToFieldElement[T emulated.FieldParams](
 	return *field.FromBits(api.ToBinary(variable)...)
 }
 
-func calculateWeights[T emulated.FieldParams](field *emulated.Field[T], xNodes []emulated.Element[T]) []emulated.Element[T] {
-	n := len(xNodes)
-	weights := make([]emulated.Element[T], n)
-
-	for j := range n {
-		w := emulated.ValueOf[T](1)
-		for k := 0; k < n; k++ {
-			if k != j {
-				w = *field.Div(&w, field.Sub(&xNodes[j], &xNodes[k]))
-			}
-		}
-		weights[j] = w
-	}
-
-	return weights
-}
-
-func barycentricPolynomial[T emulated.FieldParams](
-	field *emulated.Field[T],
-	weights, xNodes, yNodes []emulated.Element[T],
-	targetPoint emulated.Element[T],
-) emulated.Element[T] {
-	n := len(xNodes)
-	numerator := emulated.ValueOf[T](0)
-	denominator := emulated.ValueOf[T](0)
-
-	for j := range n {
-		temp := field.Div(&weights[j], field.Sub(&targetPoint, &xNodes[j]))
-		numerator = *field.Add(&numerator, field.Mul(temp, &yNodes[j]))
-		denominator = *field.Add(&denominator, temp)
-	}
-
-	return *field.Div(&numerator, &denominator)
-}
-
-type CircuitDankrad[T emulated.FieldParams] struct {
+type Circuit[T emulated.FieldParams] struct {
 	Omega big.Int // ω
 
 	// Inputs (private)
@@ -96,7 +26,7 @@ type CircuitDankrad[T emulated.FieldParams] struct {
 	InterpolatedPoint frontend.Variable `gnark:",public"`
 }
 
-func (circuit *CircuitDankrad[T]) Define(api frontend.API) error {
+func (circuit *Circuit[T]) Define(api frontend.API) error {
 	field, err := emulated.NewField[T](api)
 	if err != nil {
 		return err
@@ -113,7 +43,7 @@ func (circuit *CircuitDankrad[T]) Define(api frontend.API) error {
 	targetPointFe := variableToFieldElement(field, api, circuit.TargetPoint)
 	interpolatedPointFe := variableToFieldElement(field, api, circuit.InterpolatedPoint)
 
-	interpolatedPointCalculated := dankradBarycentricPolynomial[T](field, wFe, yNodesFe, targetPointFe)
+	interpolatedPointCalculated := calculateBarycentricFormula[T](field, wFe, yNodesFe, targetPointFe)
 
 	field.AssertIsEqual(&interpolatedPointFe, &interpolatedPointCalculated)
 
@@ -124,7 +54,7 @@ func (circuit *CircuitDankrad[T]) Define(api frontend.API) error {
 // TODO this will be 4096
 const polynomialDegree = 4
 
-func dankradBarycentricPolynomial[T emulated.FieldParams](
+func calculateBarycentricFormula[T emulated.FieldParams](
 	field *emulated.Field[T],
 	omega emulated.Element[T],
 	yNodes []emulated.Element[T],
--- prover/barycentric/barycentric_test.go
+++ prover/barycentric/barycentric_test.go
@@ -14,29 +14,6 @@ import (
 func TestBarycentricCircuit(t *testing.T) {
 	assert := test.NewAssert(t)
 
-	circuit := Circuit[emulated.BLS12381Fr]{
-		XNodes: make([]frontend.Variable, 4),
-		YNodes: make([]frontend.Variable, 4),
-	}
-
-	assignment := Circuit[emulated.BLS12381Fr]{
-		XNodes:            []frontend.Variable{1, 2, 4, 8},
-		YNodes:            []frontend.Variable{1, 8, 64, 512},
-		TargetPoint:       3,
-		InterpolatedPoint: 27,
-	}
-
-	assert.CheckCircuit(
-		&circuit,
-		test.WithBackends(backend.GROTH16),
-		test.WithCurves(ecc.BLS12_381),
-		test.WithValidAssignment(&assignment),
-	)
-}
-
-func TestDankradBarycentricCircuit(t *testing.T) {
-	assert := test.NewAssert(t)
-
 	modulus, ok := new(big.Int).SetString("52435875175126190479447740508185965837690552500527637822603658699938581184513", 10)
 	assert.True(ok)
 	omega, _ := new(big.Int).SetString("10238227357739495823651030575849232062558860180284477541189508159991286009131",
@@ -51,7 +28,7 @@ func TestDankradBarycentricCircuit(t *testing.T) {
 		omega.Mod(omega, modulus)
 	}
 
-	circuit := CircuitDankrad[emulated.BLS12381Fr]{
+	circuit := Circuit[emulated.BLS12381Fr]{
 		Omega:  *omega,
 		YNodes: make([]frontend.Variable, 4),
 	}
@@ -66,7 +43,7 @@ func TestDankradBarycentricCircuit(t *testing.T) {
 		interpolated[i] = interpolatedBI[i]
 	}
 
-	assignment := CircuitDankrad[emulated.BLS12381Fr]{
+	assignment := Circuit[emulated.BLS12381Fr]{
 		YNodes:            interpolated,
 		TargetPoint:       3,
 		InterpolatedPoint: 27,
