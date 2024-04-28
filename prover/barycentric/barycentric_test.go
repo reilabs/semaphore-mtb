570280efdd6dece97d45e3eb0a41ce0d4ee3d950
Wojciech Zmuda <zmuda.w@gmail.com>
2024-04-29 00:18:17 +0200

    turn into table-driven tests and add second test polynomial (fails)
    Signed-off-by: Wojciech Zmuda <zmuda.w@gmail.com>

--- prover/barycentric/barycentric_test.go
+++ prover/barycentric/barycentric_test.go
@@ -1,6 +1,7 @@
 package barycentric
 
 import (
+	"math"
 	"math/big"
 	"testing"
 
@@ -57,47 +58,95 @@ func (circuit *BarycentricCircuit[T]) Define(api frontend.API) error {
 func TestCalculateBarycentricFormula(t *testing.T) {
 	const polynomialDegree = 4096
 
-	assert := test.NewAssert(t)
+	// Test setup
 
 	// The test assumes BLS12381Fr field
 	modulus, _ := new(big.Int).SetString(
 		"52435875175126190479447740508185965837690552500527637822603658699938581184513", 10)
-
 
 	// For polynomial degree d = 4096 = 2^12:
 	// ω^(2^32) = ω^(2^20 * 2^12)
 	// Calculate ω^20 starting with root of unity of 2^32 degree
 	omega, _ := new(big.Int).SetString(
 		"10238227357739495823651030575849232062558860180284477541189508159991286009131", 10)
-	for range 20 {
+	polynomialDegreeExp := int(math.Log2(float64(polynomialDegree)))
+	omegaExpExp := 32  // ω^(2^32)
+	for range omegaExpExp - polynomialDegreeExp {
 		omega.Mul(omega, omega)
 		omega.Mod(omega, modulus)
 	}
 
-	circuit := BarycentricCircuit[emulated.BLS12381Fr]{
-		Omega:  *omega,
-		PolynomialDegree: polynomialDegree,
-		YNodes: make([]frontend.Variable, polynomialDegree),
+	// Test cases
+
+	type PolynomialTestCase struct {
+		Name              string
+		CalculateYNodes   func(omega *big.Int, modulus *big.Int, polynomialDegree int) []frontend.Variable
+		TargetPoint       int64
+		InterpolatedPoint int64
+	}
+	tests := []PolynomialTestCase{
+		{
+			Name: "f(x) = x^3",
+			CalculateYNodes: func(omega *big.Int, modulus *big.Int, polynomialDegree int) []frontend.Variable {
+				y := make([]frontend.Variable, polynomialDegree)
+				for i := range y {
+					y[i] = new(big.Int).Exp(omega, big.NewInt(int64(i*3)), modulus)
+				}
+				return y
+			},
+			TargetPoint:       3,
+			InterpolatedPoint: 27,
+		},
+		{
+			Name: "f(x) = 3x^7 + 2x^4 + 4x + 20",
+			CalculateYNodes: func(omega *big.Int, modulus *big.Int, polynomialDegree int) []frontend.Variable {
+				y := make([]frontend.Variable, polynomialDegree)
+				for i := range y {
+					a := new(big.Int).Exp(omega, big.NewInt(int64(i*7)), modulus)
+					a.Mul(a, big.NewInt(3))
+
+					b := new(big.Int).Exp(omega, big.NewInt(int64(i*4)), modulus)
+					b.Mul(b, big.NewInt(2))
+
+					c := new(big.Int).Exp(omega, big.NewInt(int64(i)), modulus)
+					c.Mul(c, big.NewInt(4))
+
+					res := new(big.Int).Add(a, b)
+					res.Add(res, c)
+					res.Add(res, big.NewInt(20))
+
+					y[i] = res
+				}
+				return y
+			},
+			TargetPoint:       3,
+			InterpolatedPoint: 6755,
+		},
 	}
 
-	// Test polynomial: f(x) = x^3
-	// Calculate f(ω) = ω^3 for
-	y := make([]frontend.Variable, polynomialDegree)
-	for i := range y {
-		y[i] = *new(big.Int).Exp(omega, big.NewInt(int64(i*3)), modulus)
-	}
+	for _, tc := range tests {
+		assert := test.NewAssert(t)
+		assert.Run(
+			func(a *test.Assert) {
+				circuit := BarycentricCircuit[emulated.BLS12381Fr]{
+					Omega:            *omega,
+					PolynomialDegree: polynomialDegree,
+					YNodes:           make([]frontend.Variable, polynomialDegree),
+				}
 
-	assignment := BarycentricCircuit[emulated.BLS12381Fr]{
-		YNodes:            y,
-		// f(3)=3^3=27
-		TargetPoint:       3,
-		InterpolatedPoint: 27,
-	}
+				assignment := BarycentricCircuit[emulated.BLS12381Fr]{
+					YNodes:            tc.CalculateYNodes(omega, modulus, polynomialDegree),
+					TargetPoint:       tc.TargetPoint,
+					InterpolatedPoint: tc.InterpolatedPoint,
+				}
 
-	assert.CheckCircuit(
-		&circuit,
-		test.WithBackends(backend.GROTH16),
-		test.WithCurves(ecc.BLS12_381),
-		test.WithValidAssignment(&assignment),
-	)
+				assert.CheckCircuit(
+					&circuit,
+					test.WithBackends(backend.GROTH16),
+					test.WithCurves(ecc.BLS12_381),
+					test.WithValidAssignment(&assignment),
+				)
+			}, tc.Name,
+		)
+	}
 }
