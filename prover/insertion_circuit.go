936cd20dda7eded8ff024c71696b96bbf9165f21
Wojciech Zmuda <zmuda.w@gmail.com>
2024-05-28 00:04:51 +0300

    input hash is sometimes 253 sometimes 254 bits, need to fix

--- prover/insertion_circuit.go
+++ prover/insertion_circuit.go
@@ -120,7 +120,7 @@ func (circuit *InsertionMbuCircuit) Define(api frontend.API) error {
 	bitsHash := abstractor.Call1(
 		api, ToReducedBigEndian{
 			Variable: circuit.InputHash,
-			Size:     32,
+			Size:     253,  // TODO make sure this length is always right
 		},
 	)
 	bits = append(bits, bitsHash...)
